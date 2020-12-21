30153b1f1b610dd896902779889917df51f6e73b
Anna G <anngoncharova08@gmail.com>
2020-12-21 14:30:53 +0300

    Simplified ModInverse
    Made use of BigInteger method ModPow and Euler's theorem.

--- Gelfond-Silver-Pohlig-Hellman/BigIntegerExtensions.cs
+++ Gelfond-Silver-Pohlig-Hellman/BigIntegerExtensions.cs
@@ -4,21 +4,11 @@ namespace GSPH
 {
     public static class BigIntegerExtensions
     {
+
         public static BigInteger ModInverse(this BigInteger a, BigInteger m)
         {
-            if (m == 1) return 0;
-            var m0 = m;
-            (BigInteger x, BigInteger y) = (1, 0);
-
-            if (a < 0) return m - ModInverse(-a, m);
-
-            while (a > 1)
-            {
-                var q = a / m;
-                (a, m) = (m, a % m);
-                (x, y) = (y, x - q * y);
-            }
-            return x < 0 ? x + m0 : x;
+            var aInverse = BigInteger.ModPow(a, m - 2, m);
+            return aInverse < 0 ? aInverse + m : aInverse;
         }
     }
 }
